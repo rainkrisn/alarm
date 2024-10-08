2c817a627444b2614e78b51b52764cba5171342b
rainkrisn <aathrey.krishnan@gmail.com>
2024-10-08 03:16:16 -0700

    messing with date & time

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from tkinter import *
-import time as time_module
+import time 
 from playsound import playsound
 from PIL import Image, ImageTk
 
 root = Tk()
+
+root.title("Alarm Clock - Made by Rain")
+root.geometry("400x500")
+
+# load the image
+image_path = r'c:\Users\aniru\Downloads\alarm clock.png'
+img_open = Image.open(image_path)
+img_resized = img_open.resize((25, 25))  
+img = ImageTk.PhotoImage(img_resized)
+
+# create a Label with both text and image
+alarm_welcome = Label(root, text="Alarm Clock", font=("Consolas", 15, "bold"), compound=LEFT, image=img)
+alarm_welcome.pack()
+
+# author name etc
+byline = Label(root, text="Made by Rain", font=("Consolas", 10, 'italic'))
+byline.place(x=150, y=25)
 
 def time():
     time_in_mins = dropdown.get() # taking the input from user
@@ -24,23 +41,11 @@ def time():
 def ringtone():
     playsound(r'c:\Users\aniru\Downloads\Nokia ringtone arabic.mp3')
 
+def countdown():
+    time_in_mins = dropdown.get()
+    for i in range(time_in_mins, 0, -1):
+        print ('hi')
 
-root.title("Alarm Clock - Made by Rain")
-root.geometry("400x500")
-
-# load the image
-image_path = r'c:\Users\aniru\Downloads\alarm clock.png'
-img_open = Image.open(image_path)
-img_resized = img_open.resize((25, 25))  
-img = ImageTk.PhotoImage(img_resized)
-
-# create a Label with both text and image
-alarm_welcome = Label(root, text="Alarm Clock", font=("Consolas", 15, "bold"), compound=LEFT, image=img)
-alarm_welcome.pack()
-
-# author name etc
-byline = Label(root, text="Made by Rain", font=("Consolas", 10, 'italic'))
-byline.place(x=150, y=25)
 
 label = Label(root, text = "Enter the time: ", font = ("Consolas", 10, 'bold'))
 label.place(x=30, y=100)
